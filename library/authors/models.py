cb6a719a3e550277d5a479d11a98427e0d0201de
Jenny6199 <Jenny6199@yandex.ru>
2022-02-22 15:12:16 +0300

    add UUID in model Author

--- library/authors/models.py
+++ library/authors/models.py
@@ -1,15 +1,18 @@
 from django.db import models
-
+from uuid import uuid4
 
 class Author(models.Model):
     """
     Класс Автор
     Общая, простая модель автора книги\n
     ----------------------------------\n
+    uid - первичный ключ, UUID\n
     first_name - текст (макс.длина 64 символа)\n
     last_name - текст (макс.длина 64 символа)\n
     birthday_year - натуральное число\n
+    ----------------------------------\n
     """
+    uid = models.UUIDField(primary_key=True, default=uuid4)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     birthday_year = models.PositiveIntegerField()
