e4202e465a052360ff7cf12fbc7e3f7a80c59943
Jenny6199 <Jenny6199@yandex.ru>
2022-02-22 14:57:06 +0300

    create model Author

--- library/authors/models.py
+++ library/authors/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    """
+    Класс Автор
+    Общая, простая модель автора книги\n
+    ----------------------------------\n
+    first_name - текст (макс.длина 64 символа)\n
+    last_name - текст (макс.длина 64 символа)\n
+    birthday_year - натуральное число\n
+    """
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    birthday_year = models.PositiveIntegerField()
