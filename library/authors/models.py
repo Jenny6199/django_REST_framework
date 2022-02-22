9eaee07a89f167c1268d78fcfd619ab36401ad62
Jenny6199 <Jenny6199@yandex.ru>
2022-02-22 21:24:56 +0300

    create authors/serializers.py

--- library/authors/models.py
+++ library/authors/models.py
@@ -4,13 +4,13 @@ from uuid import uuid4
 class Author(models.Model):
     """
     Класс Автор
-    Общая, простая модель автора книги\n
-    ----------------------------------\n
-    uid - первичный ключ, UUID\n
-    first_name - текст (макс.длина 64 символа)\n
-    last_name - текст (макс.длина 64 символа)\n
-    birthday_year - натуральное число\n
-    ----------------------------------\n
+    Общая, простая модель автора книги
+    ----------------------------------
+    uid - первичный ключ, UUID
+    first_name - текст (макс.длина 64 символа)
+    last_name - текст (макс.длина 64 символа)
+    birthday_year - натуральное число
+    ----------------------------------
     """
     uid = models.UUIDField(primary_key=True, default=uuid4)
     first_name = models.CharField(max_length=64)
