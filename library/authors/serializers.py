673bcb206d7c35338b64b6f4013b86a5c0dcb692
Jenny6199 <Jenny6199@yandex.ru>
2022-02-22 21:34:15 +0300

    create AuthorModelViewSet in authors/views.py

--- library/authors/serializers.py
+++ library/authors/serializers.py
@@ -3,6 +3,10 @@ from .models import Author
 
 
 class AuthorModelSerializer(HyperlinkedModelSerializer):
+    """
+    Сериализатор для модели Author,
+    наследуется от HyperlinkedModelSerializer,
+    """
     class Meta:
         model = Author
         fields = '__all__'
--- library/authors/views.py
+++ library/authors/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import ModelViewSet
+from .model import Author
+from .serializers import AuthorModelSerializer
 
-# Create your views here.
+
+class AuthorModelViewSet(ModelViewSet):
+    """
+    Класс набора представлений для модели Author
+    """
+    queryset = Author.objects.all()
+    serializer_class = AuthorModelSerializer
