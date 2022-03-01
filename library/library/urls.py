bb587c89b6ee7e45e1a0f9294e0afb5f54b5853f
Jenny6199 <Jenny6199@yandex.ru>
2022-03-02 01:37:35 +0300

    register Book-, Article-, BiographyViewSet in library/urls.py

--- library/library/urls.py
+++ library/library/urls.py
@@ -17,14 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from authors.views import AuthorModelViewSet
+from authors.views import AuthorModelViewSet, BiographyModelViewSet, ArticleModelViewSet, BookModelViewSet
 
 router = DefaultRouter()
 router.register('authors', AuthorModelViewSet)
+router.register('books', BookModelViewSet)
+router.register('articles', ArticleModelViewSet)
+router.register('biographies', BiographyModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
 ]
-
