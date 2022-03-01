17f1d6e8395c7b001bad707362316299d0bbb782
Jenny6199 <Jenny6199@yandex.ru>
2022-03-02 01:25:05 +0300

    add viewsets for Book, Article, Biography

--- library/authors/views.py
+++ library/authors/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .models import Author
-from .serializers import AuthorModelSerializer
+from .models import Author, Biography, Book, Article
+from .serializers import AuthorModelSerializer, BiographyModelSerializer, ArticleModelSerializer, BookModelSerializer
 
 
 class AuthorModelViewSet(ModelViewSet):
@@ -10,3 +10,27 @@ class AuthorModelViewSet(ModelViewSet):
     """
     queryset = Author.objects.all()
     serializer_class = AuthorModelSerializer
+
+
+class BiographyModelViewSet(ModelViewSet):
+    """
+    Класс набора представлений для модели Biography
+    """
+    queryset = Biography.objects.all()
+    serializer_class = BiographyModelSerializer
+
+
+class ArticleModelViewSet(ModelViewSet):
+    """
+    Класс набора представлений для модели Article
+    """
+    queryset = Article.objects.all()
+    serializer_class = ArticleModelSerializer
+
+
+class BookModelViewSet(ModelViewSet):
+    """
+    Класс набора представлений для модели Book
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookModelSerializer
