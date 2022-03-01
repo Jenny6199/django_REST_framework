87dc257a4ec93ab4a1bee65ea1c30cbd59cb93b5
Jenny6199 <Jenny6199@yandex.ru>
2022-03-02 00:38:30 +0300

    add models Biography, Book, Article in authors/models.py

--- library/authors/models.py
+++ library/authors/models.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 
 class Author(models.Model):
     """
-    Класс Автор
+    Класс Автор.
     Общая, простая модель автора книги
     ----------------------------------
     uid - первичный ключ, UUID
@@ -16,3 +16,37 @@ class Author(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     birthday_year = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.firs_name + ' ' + self.last_name
+
+    
+class Biography(models.Model):
+    """
+    Класс Биография.
+    Простая модель биографии автора, содержит связь one-to-one с классом автора
+    (биография автора)
+    ---------------------------------
+    """
+    text = models.TextField()
+    author = models.OneToOneField(Author, on_delete=models.CASCADE)
+
+
+class Book(models.Model):
+    """
+    Класс Книга.
+    Простая модель книги, содержит связь many-to-many с классом автора
+    (может быть написана несколькими авторами)
+    """
+    book_name = models.CharField(max_length=64)
+    authors = models.ManyToManyField(Author)
+
+
+class Article(models.Model):
+    """
+    Класс Статья.
+    Модель статьи, содержит внешний ключ для связи с конкретным автором
+    (статья написана автором)
+    """
+    article_name = models.CharField(max_length=64)
+    author = models.ForeignKey(Author, models.PROTECT)
