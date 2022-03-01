1784d0138ecd7a352bab9f0091dbe8d9a30800c5
Jenny6199 <Jenny6199@yandex.ru>
2022-03-02 01:08:35 +0300

    add Biography- Book- and ArticleSerializer in authors/serializers.py

--- library/authors/serializers.py
+++ library/authors/serializers.py
@@ -1,13 +1,52 @@
+from rest_framework.serializers import ModelSerializer, StringRelatedField
 from rest_framework.serializers import HyperlinkedModelSerializer
-from .models import Author
+from .models import Article, Author, Biography, Book
 
 
 class AuthorModelSerializer(HyperlinkedModelSerializer):
     """
     Сериализатор для модели Author,
     наследуется от HyperlinkedModelSerializer,
+    поля - все поля модели автора.
     """
     class Meta:
         model = Author
+        fields = '__all__'
+
+
+class BiographyModelSerializer(ModelSerializer):
+    """
+    Сериализатор для модели Biography,
+    наследуется от ModelSerializer,
+    поля - text, author
+    """
+    class Meta:
+        model = Biography
+        fields = ['text', 'author']
+
+
+class ArticleModelSerializer(ModelSerializer):
+    """
+    Сериализатор для модели Article, 
+    наследуется от ModelSerializer,
+    поля - исключено поле name
+    """
+    author = AuthorModelSerializer()
+
+    class Meta:
+        model = Article
+        exclude = ['article_name']
+
+
+class BookModelSerializer(ModelSerializer):
+    """
+    Сериализатор для модели Book,
+    наследуется от ModelSerializer, 
+    поля - все поля.
+    """
+    authors = StringRelatedField(many=True)
+
+    class Meta:
+        model = Book
         fields = '__all__'
 
