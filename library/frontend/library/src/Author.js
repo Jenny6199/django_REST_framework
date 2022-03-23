3332b399bd60012c823514e98cad2e138e5efc16
Jenny6199 <Jenny6199@yandex.ru>
2022-03-23 19:14:03 +0300

    create-react-app and add Author.js

---
+++ library/frontend/library/src/Author.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const AuthroItem = ({item}) => {
+    return (
+        <tr>
+            <td>{item.id}</td>
+            <td>{item.name}</td>
+            <td>{item.birthday_year}</td>
+        </tr>
+    )
+}
+
+const AuthorLIst = ({items}) => {
+    return (
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>BIRTHDAY_YEAR</th>
+            </tr>
+            {items.map((item) => <AuthroItem item={item} />)}
+        </table>
+    )
+}
+
+export default AuthorLIst
